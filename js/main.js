3734511102133a05ad50d36f475b3a903359548c
Joe Manto <joemanto123@gmail.com>
2019-04-11 22:59:25 -0400

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-
 $(function() {
     //no cheating
-    var flag = $.ajax({type: "GET", url: "https://raw.githubusercontent.com/JoeManto/BarcodeCTF/master/answer.txt", async: false}).responseText;
-
+    var flag = $.trim($.ajax({type: "GET", url: "https://raw.githubusercontent.com/JoeManto/BarcodeCTF/master/answer.txt", async: false}).responseText);
+    console.log(flag);
     let c = s2(s1(350))- Math.sqrt(Infinity);
     let v = config(c,"0",1,0,0);
     $('p:first').text(function() {return v});
     $( "#check" ).click(function() {
+        console.log($("#input").val());
        if(flag === $("#input").val()){
            console.log("good job");
        }else{
@@ -14,9 +14,6 @@ $(function() {
        }
     });
 });
-
-
-
 function config(n,seq,x0,x1,temp){
     if(n<=1) n = s2(s1(350))-s1(999)+6;
     while (n >= 0){
@@ -28,16 +25,9 @@ function config(n,seq,x0,x1,temp){
     }
     return seq;
 }
-
-function checkFlag(flag,input) {
-
-}
-
 function s1(num){
     for(;num>=8;num--){num = num<<num;}return Math.abs(num);
 }
 function s2(num){
     for(;num<=8;num++){num = num>>num;}return num;
 }
-
-
